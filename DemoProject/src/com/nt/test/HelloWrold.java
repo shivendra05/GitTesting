668b438226c16cc389afc7f5af946ee3fc296850
Team2 <shivendrapandey2@gmail.com>
2020-05-14 14:55:53 +0530

    Team1 Modifyed

--- DemoProject/src/com/nt/test/HelloWrold.java
+++ DemoProject/src/com/nt/test/HelloWrold.java
@@ -2,9 +2,12 @@ package com.nt.test;
 
 public class HelloWrold {
 
+	public static int sum(int a,int b) {
+		return a+b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, this is TL");
+		HelloWrold.sum(1, 20);
 	}
-
 }
