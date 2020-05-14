56f711deab2b10bad0ad4a19d0c5588c54cecf14
Team2 <shivendrapandey2@gmail.com>
2020-05-14 15:13:42 +0530

    Team2 Adding Data

--- DemoProject/src/com/nt/test/HelloWrold.java
+++ DemoProject/src/com/nt/test/HelloWrold.java
@@ -5,9 +5,15 @@ public class HelloWrold {
 	public static int sum(int a,int b) {
 		return a+b;
 	}
+	public static int mul(int a,int b) {
+		return a*b;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, this is TL");
+		System.out.println("Add-----------");
 		HelloWrold.sum(1, 20);
+		System.out.println("Muliply--------------");
+		HelloWrold.mul(10, 20);
 	}
 }
