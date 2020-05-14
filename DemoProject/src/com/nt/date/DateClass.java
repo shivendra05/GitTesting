97e1654096fdb1b5211336315541e243256631e3
Team2 <shivendrapandey2@gmail.com>
2020-05-14 14:46:11 +0530

    Added hello world and Date Classes

---
+++ DemoProject/src/com/nt/date/DateClass.java
@@ -0,0 +1,16 @@
+package com.nt.date;
+
+import java.util.Date;
+
+public class DateClass {
+
+	public Date getDate() {
+		return new Date();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DateClass d = new DateClass();
+		System.out.println(d.getDate());
+	}
+
+}
