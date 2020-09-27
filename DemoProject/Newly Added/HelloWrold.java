4de3b919ab2caec70d3d51aee922cb602979ad9c
shivendra <shivendrapandey786@gmail.com>
2020-09-27 19:09:17 +0530

    a111 sql file and java file

---
+++ DemoProject/Newly Added/HelloWrold.java
@@ -0,0 +1,18 @@
+package com.nt.test;
+
+public class HelloWrold {
+
+	public static int sum(int a,int b) {
+		return a+b;
+	}
+	public static int mul(int a,int b) {
+		return a*b;
+	}
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println("Hello World");
+	}
+}
