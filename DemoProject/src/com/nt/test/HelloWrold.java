67851a7c9a706fcd4cd01a46d18670c7fa713e8c
shivendra <shivendrapandey786@gmail.com>
2020-09-27 19:39:02 +0530

    update data

--- DemoProject/src/com/nt/test/HelloWrold.java
+++ DemoProject/src/com/nt/test/HelloWrold.java
@@ -9,13 +9,8 @@ public class HelloWrold {
 		return a*b;
 	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, this is TL");
-		System.out.println("Add-----------");
-		HelloWrold.sum(1, 20);
-		System.out.println("Muliply--------------");
-		HelloWrold.mul(10, 20);
 		
 		System.out.println("Hi This is for confilit------I am Team2");
+System.out.println("new from Sprint 2");
 	}
 }
